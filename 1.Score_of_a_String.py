221a0f2356a66f7f22f11181d35e70bc5ea6a8de
Himanshu Jaiswal <55348957+itshimanshu010@users.noreply.github.com>
2026-01-14 19:41:17 +0530

    Create 1.Score_of_a_String.py

---
+++ 1.Score_of_a_String.py
@@ -0,0 +1,17 @@
+# Example 1:
+
+# Input: s = "code"
+# Output: 24
+# Explanation: The ASCII values of the characters in the given string are: 'c' = 99, 'o' = 111, 'd' = 100, and 'e' = 101. The score of s will be: |111 - 99| + |100 - 111| + |101 - 100|.
+
+#Code:
+
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+
+class Solution:
+    def scoreOfString(self, s:str) -> int:
+        res = 0;
+        for i in range(len(s) - 1):
+            res += abs(ord(s[i]) - ord(s[i+1]))
+        return res
